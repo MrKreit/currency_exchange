5be4c0f7d23dbb3ff2734cdf7094944c1065e187
Maryna Matsveyenka <marina.matweenko1994@gmail.com>
2024-02-04 16:35:15 +0300

    add main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
-
   public static void main(String[] args) {
+    CurrencyExchangeApp currencyExchangeApp = new CurrencyExchangeApp();
 
+    try {
+      Scanner scanner = new Scanner(System.in);
+
+      while (true) {
+        currencyExchangeApp.displayMenu();
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (choice) {
+          case 1 -> currencyExchangeApp.performExchange(scanner);
+          case 2 -> currencyExchangeApp.viewExchangeHistory();
+          case 3 -> {
+            System.out.println("Выход из программы. До свидания!");
+            System.exit(0);
+          }
+          default -> System.out.println("Неверный выбор. Пожалуйста, выберите снова.");
+        }
+      }
+    } catch (InputMismatchException e) {
+      System.out.println("Ошибка ввода. Введите число.");
+    }
   }
 }

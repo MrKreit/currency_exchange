d824720754e50688ea92d2475954ddc04c0f2fde
Maryna Matsveyenka <marina.matweenko1994@gmail.com>
2024-02-07 21:46:54 +0300

    changed CurrencyExchangeApp and ExchangeManager

--- src/CurrencyExchangeApp.java
+++ src/CurrencyExchangeApp.java
@@ -1,47 +1,47 @@
 import java.util.Scanner;
+
 // Класс CurrencyExchangeApp представляет собой консольное приложение
 // для взаимодействия с пользователем.
-// Этот класс является частью пользовательского интерфейса и связывает методы из класса ExchangeManager
+// Этот класс является частью пользовательского интерфейса и связывает
+// методы из класса ExchangeManager
 // с консольным вводом/выводом, обеспечивая взаимодействие пользователя с функциональностью обмена валют.
+
+// private final ExchangeManager exchangeManager = new ExchangeManager();
+// - создает объект ExchangeManager, который будет использоваться для управления обменом валют
+// и хранения истории обменов.
+// Этот объект инициализируется в конструкторе класса.
+
+
+// public void displayMenu() - выводит меню приложения на экран. Пользователь видит опции для выбора: обмен валюты,
+// просмотр истории обменов или выход из приложения.
 public class CurrencyExchangeApp {
 
-  // private final ExchangeManager exchangeManager = new ExchangeManager();
-  // - создает объект ExchangeManager, который будет использоваться для управления обменом валют
-  // и хранения истории обменов.
-  // Этот объект инициализируется в конструкторе класса.
   private final ExchangeManager exchangeManager = new ExchangeManager();
-
-  // public void displayMenu() - выводит меню приложения на экран. Пользователь видит опции для выбора: обмен валюты,
-  // просмотр истории обменов или выход из приложения.
 
   public void displayMenu() {
     System.out.println("\nМеню:");
     System.out.println("1. Обмен валюты");
-    System.out.println("2. Просмотр истории обменов");
+    System.out.println("2. История обменов");
     System.out.println("3. Выход");
     System.out.print("Выберите опцию: ");
   }
-  public void saveHistoryToFile(Scanner scanner) {
-    System.out.print("Введите имя файла для сохранения истории: ");
-    String fileName = scanner.next();
-    exchangeManager.saveHistoryToFile(fileName);
-    System.out.println("История успешно сохранена в файле " + fileName);
+  public void displayCurrencyAbbreviations() {
+    System.out.println("Доступные валюты:");
+    for (CurrencyEnum currency : CurrencyEnum.values()) {
+      System.out.println(currency.name() + ": " + currency.getDescription());
+    }
   }
 
-  // public void performExchange(Scanner scanner) - предоставляет пользователю возможность
-  // выполнить обмен валюты. Сначала отображает доступные валюты с их сокращениями,
-  // а затем вызывает метод performExchange объекта ExchangeManager,
-  // передавая ему сканнер для ввода данных пользователем.
-
   public void performExchange(Scanner scanner) {
-    exchangeManager.displayCurrencyAbbreviations();
+
     exchangeManager.performExchange(scanner);
   }
 
-  // public void viewExchangeHistory() - предоставляет пользователю возможность просмотреть историю обменов.
-  // Вызывает метод viewExchangeHistory объекта ExchangeManager.
+
 
   public void viewExchangeHistory() {
-    exchangeManager.viewExchangeHistory();
+    exchangeManager.viewExchangeHistoryFromFile();
   }
+
+
 }
